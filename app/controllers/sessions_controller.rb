a428d31f5dc2fd14059f1ff031eaaf2ed299b505
Zhanar <zhanaomir@gmail.com>
2014-08-15 15:02:14 +0000

    Sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+     user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
+  else
+    flash.now[:error]= "неверный логин и/или пароль"
+    render 'new'
+    # Create an error message and re-render the signin form.
+  end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
+       
+#  get 'users/new'
+
    root 'static_pages#home'
 
-
+  match '/signup',  to: 'users#new',       via: :get
+  match '/signin', to: 'sessions#new',     via: :get
   match '/about' , to:'static_pages#about', via: :get 
-
+  match '/contact', to: 'static_pages#contact', via: :get
   match '/help' , to:'static_pages#help', via: :get 
-
-
+  match '/signout', to: 'sessions#destroy', via: :delete
+ 
+  resources :sessions, only:[ :new, :create, :destroy]
   resources :microposts
+  resources :users  
+  
 
-  resources :users
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
