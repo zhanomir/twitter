29d2e656eb8dac4cb77817efb9a02a621ce870b4
Zhanar <zhanaomir@gmail.com>
2014-08-19 04:33:50 +0000

    Edit forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,67 @@
 class UsersController < ApplicationController
-  def new
-    
+  before_action :signed_in_user, only: [:edit, :update, :index]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only:[:destroy]
+ def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+  def index
+    @users= User.paginate(page: params[:page])
   end
   def show
     @user = User.find(params[:id])
   end
-def new
+  
+  def new
     @user = User.new
-   end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:succes]= "Регистрация прошла успешно!"
+      sign_in @user
+      flash[:success] = "Welcome to Twitter!"
       redirect_to @user
-      
-    else 
+    else
       render 'new'
-  end
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
-  def user_params
+
+    def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
 
-
-
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+  
+end
